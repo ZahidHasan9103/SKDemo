91d4f95e5d7bdf06c49a145a7cab6d380cd3e267
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-08-07 18:26:29 +0600

    added storeKit configuration file + info.plist to store all the product identifiers

---
+++ SKDemo/Shop/ShopViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ShopViewController.swift
+//  SKDemo
+//
+//  Created by ZEUS on 6/8/24.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ShopViewController: UITableViewDataSource, UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
